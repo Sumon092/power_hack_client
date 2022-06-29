136064ed269a6830eef21d744e9245b49191ec06
sumon092 <m.sumon@outlook.com>
2022-06-30 04:16:10 +0600

    project initialize

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#bde886",
+
+          "secondary": "#c0dff9",
+
+          "accent": "#ea79c8",
+
+          "neutral": "#2A2131",
+
+          "base-100": "#262740",
+
+          "info": "#5B9EF5",
+
+          "success": "#0F8544",
+
+          "warning": "#C57316",
+
+          "error": "#E93541",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
